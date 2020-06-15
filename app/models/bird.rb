ec2b89cfa3884b1ffa666070da0ba52192b96c03
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-15 00:12:27 +0000

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,1 +1,11 @@
-# Add code from Readme
+# Add code from Readme# Add code from Readme
+class Bird < ApplicationRecord
+end
+Rails.application.routes.draw do
+  get '/birds' => 'birds#index'
+end
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+  end
+end
